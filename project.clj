f76e4d56670e74d3b1bb16cb9ef74b64644f1a25
Tassilo Horn <tsdh@gnu.org>
2013-06-18 18:14:02 +0200

    initial import

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject ttc-2013-live "0.1.0-SNAPSHOT"
+  :description "FunnyQT solution to the TTC'13 live contest."
+  :url "http://example.com/FIXME"
+  :license {:name "GNU General Public License v3 (or later)"
+            :url "http://www.gnu.org/licenses/gpl.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [funnyqt "0.4.28"]])
---
+++ src/ttc_2013_live/core.clj
@@ -0,0 +1,6 @@
+(ns ttc-2013-live.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
